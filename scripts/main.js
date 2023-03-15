44845f4a8e2f28761f04ac236102cb47b68b98fb
foxHound <vanbeek.jos@gmail.com>
2023-03-15 15:06:57 +1300

    first commit yoz

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+/*global $, jQuery, console, window*/
+
+var Main = (function () {
+  "use strict";
+  var pub = {};
+
+
+
+
+        pub.setup = function () {
+        };
+        return pub;
+        }());
+
+        $(document).ready(Main.setup);
